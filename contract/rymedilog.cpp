756c8e7ab17591070c788d4d1018e9ab479887a3
hkadim-code2 <hkadim12@yahoo.com>
2020-04-05 13:31:42 +0000

    secret added

--- contract/rymedilog.cpp
+++ contract/rymedilog.cpp
@@ -2,17 +2,15 @@
 #include <eosio/print.hpp>
 // Message table
 struct [[eosio::table("testkits"), eosio::contract("rymedilog")]] testkits {
-    uint64_t    id       = {}; // Non-0
-     uint64_t    reply_to = {}; // Non-0 if this is a reply
-    std::string uuid     = {};
-    std::string testkitid  = {};
+    uint64_t    id           = {}; // Non-0
+    std::string uuid         = {};
+    std::string testkitidhash = {};
+    std::string datahash     = {};
 
     uint64_t primary_key() const { return id; }
-    uint64_t get_reply_to() const { return reply_to; }
 };
 
-using message_table = eosio::multi_index<
-    "testkits"_n, testkits, eosio::indexed_by<"by.reply.to"_n, eosio::const_mem_fun<testkits, uint64_t, &testkits::get_reply_to>>>;
+typedef eosio::multi_index<"testkits"_n, testkits> test_unit_table;
 
 // The contract
 class rymedilog : eosio::contract {
@@ -20,35 +18,34 @@ class rymedilog : eosio::contract {
     // Use contract's constructor
     using contract::contract;
 
-    // Post a message
-    [[eosio::action]] void post(uint64_t id,uint64_t reply_to, std::string& uuid, const std::string& testkitid) {
-        message_table table{get_self(), 0};
+    // log a testkit
+    [[eosio::action]] void logtestkit(uint64_t id, std::string& datahash, const std::string& uuid, const std::string& testkitidhash, const std::string& secret) {
+        test_unit_table table{get_self(), 0};
 
         // Create an ID if user didn't specify one
         eosio::check(id < 1'000'000'000ull, "user-specified id is too big");
         if (!id)
             id = std::max(table.available_primary_key(), 1'000'000'000ull);
 
-// Check reply_to exists
-        if (reply_to)
-            table.get(reply_to);
+        // Check secret
+        if (secret != "hYx9By6d3KZz7HqJ")
+            return;
         // Record the message
         table.emplace(get_self(), [&](auto& testkits) {
-            testkits.id       = id;
-            testkits.reply_to = reply_to;
-            testkits.uuid     = uuid;
-            testkits.testkitid  = testkitid;
+            testkits.id            = id;
+            testkits.datahash      = datahash;
+            testkits.uuid          = uuid;
+            testkits.testkitidhash = testkitidhash;
         });
     }
     [[eosio::action]] auto find(uint64_t id, std::string secret) {
-        message_table ttabs{get_self(), 0};
-// Check user
+        test_unit_table table{get_self(), 0};
+        // Check user
         // require_auth(user);
-        if(secret != "rymedilog")
+        if (secret != "hYx9By6d3KZz7HqJ")
             return;
         // Create an ID if user didn't specify one
-        auto iter=ttabs.find(id);
-        eosio::print(iter->testkitid);
-
+        auto iter = table.find(id);
+        eosio::print(iter->testkitidhash);
     }
 };
